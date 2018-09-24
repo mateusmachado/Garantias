1f06dd737f9f1c8354fa2dbd7ddb570fb957fe83
Mateus <mmachado.csp@gmail.com>
2018-09-24 16:18:28 -0300

    initial commit

---
+++ src/main/java/br/com/badesul/garantia/GarantiasApplication.java
@@ -0,0 +1,12 @@
+package br.com.badesul.garantia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarantiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarantiasApplication.class, args);
+	}
+}
